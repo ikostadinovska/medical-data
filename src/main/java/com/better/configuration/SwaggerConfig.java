36fa2ded15bcbcd10b0d74e0ad5017c85644c8f9
kostadinovskai <iki.kostadinovska@gmail.com>
2019-12-16 23:33:43 +0100

    add swagger;

---
+++ src/main/java/com/better/configuration/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.better.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Ivana Kostadinovska on 16-Dec-19.
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.better"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
--- src/main/java/com/better/rest/MedicalDataService.java
+++ src/main/java/com/better/rest/MedicalDataService.java
@@ -1,6 +1,7 @@
 package com.better.rest;
 
 import com.better.model.dto.Doctor;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
  */
 public interface MedicalDataService {
 
-//    @ApiOperation(value = "Add new document.", notes = "Method adds new document.")
-    @PostMapping(value = "/v1/ad-document",
+    @ApiOperation(value = "Add new document.", notes = "Method adds new document.")
+    @PostMapping(value = "/v1/add-document",
             consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
     ResponseEntity addDocument(@RequestBody Doctor doctor);
 }
