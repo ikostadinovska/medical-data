18c3d347cc132befd7acd8059b241607fd527631
kostadinovskai <iki.kostadinovska@gmail.com>
2019-12-16 23:00:08 +0100

    add xml binder;

--- src/main/java/com/better/model/dto/Doctor.java
+++ src/main/java/com/better/model/dto/Doctor.java
@@ -2,15 +2,28 @@ package com.better.model.dto;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.*;
 import java.util.List;
 
 /**
  * Created by Ivana Kostadinovska on 16-Dec-19.
  */
 @Data
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "id",
+        "department",
+        "patients"
+})
+@XmlRootElement(name = "doctor")
 public class Doctor {
+
+    @XmlAttribute(name="id")
     private Long id;
+    @XmlAttribute(name="department")
     private String department;
 
+    @XmlElementWrapper(name="patients")
+    @XmlElement(name = "patient")
     private List<Patient> patients;
 }
--- src/main/java/com/better/model/dto/Patient.java
+++ src/main/java/com/better/model/dto/Patient.java
@@ -2,16 +2,31 @@ package com.better.model.dto;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.*;
 import java.util.List;
 
 /**
  * Created by Ivana Kostadinovska on 16-Dec-19.
  */
 @Data
-public class Patient {
-    private Long id;
-    private String first_name;
-    private String last_name;
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "id",
+        "firstName",
+        "lastName",
+        "diseases"
 
+})
+@XmlRootElement(name = "patient")
+public class Patient {
+    @XmlElement(name = "id")
+    private Long id;
+    @XmlElement(name = "first_name")
+    private String firstName;
+    @XmlElement(name = "last_name")
+    private String lastName;
+
+    @XmlElementWrapper(name = "diseases")
+    @XmlElement(name = "disease")
     private List<Disease> diseases;
 }
